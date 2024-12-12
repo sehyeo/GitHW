13fa6280ea65007488479a633d2c0f9e90e8571d
벨라 <icomksh@gmail.com>
2024-12-12 22:48:07 +0900

    modify in devB branch

--- GitHW/src/githw/A.java
+++ GitHW/src/githw/A.java
@@ -4,9 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		 System.out.println("Hello Git!");
 		 System.out.println("This is A.java");
-
 		 System.out.println("modify A in devB branch");
-		 System.out.println("modify A in master branch");
 
 	}
 }
